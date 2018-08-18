d455d395ba0340173050642a6c467c374ac798f9
aayushdutt <aayushd100@gmail.com>
2018-08-19 04:17:11 +0530

    setup Contentful, fetch blog posts

--- src/app/Blog.js
+++ src/app/Blog.js
@@ -1,5 +1,33 @@
 import React from 'react'
-const Blog = () => (
-  <p>This is the Blog Page</p>
-)
+import * as contentful from 'contentful'
+class Blog extends React.Component {
+  state = {
+    posts: []
+  }
+  client = contentful.createClient({
+    space: '9ksehkkm45mk',
+    accessToken: '64b68a652631cbec8a7cc361ff42f31b26c5eeaeebba739e88dc58e94e6f2ce0'
+  })
+  componentDidMount() {
+    this.fetchPosts().then(this.setPosts);
+  }
+  fetchPosts = () => this.client.getEntries()
+  setPosts = response => {
+    this.setState({
+      posts: response.items
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <p>This is the Blog Page</p>
+        <br/>
+        { this.state.posts.map(({fields}, i) =>
+          <pre key={i}>{JSON.stringify(fields, null, 2)}</pre>
+        )}
+      </div>
+    )
+  }
+}
 export default Blog
